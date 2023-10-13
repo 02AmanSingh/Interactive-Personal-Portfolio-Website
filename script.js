6c61b628a7076ee65487fa1d064ad368b2d68561
02AmanSingh <amansingh02001@gmail.com>
2023-10-13 14:54:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+/*********************Toggle Icon Navbar********************/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+/*********************Scroll Section Active Link********************/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /*********************Sticky Navbar********************/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*********************Remove Toggle Icon Navbar when click navbar link(scroll)********************/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/*********************Sticky Navbar********************/
+ScrollReveal({
+    // reset:true,
+    distance: '80px',
+    duration: 3000,
+    delay: 20, 
+});
+
+
+
+ScrollReveal().reveal('.home-content ', {origin: 'left'});
+ScrollReveal().reveal('.heading ', {origin: 'top'});
+ScrollReveal().reveal('.about-content, .services-container, .skills-container, .projects-container, .contact form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-img ', {origin: 'right'});
+ScrollReveal().reveal('.about-img ', {origin: 'left'});
+
+/*********************typed js********************/
+const typed = new Typed('.multiple-Text', {
+    strings: ['Web Developer' , 'Android Developer' , 'Open Source Contributer' , 'FreeLancer'],
+    typeSpeed: 150,
+    backSpeed: 150,
+    backDelay: 1200,
+    loop: true,
+});
+
+
+
+
